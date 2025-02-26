c4415f3f2f6a37b93b2ff36f3e83e9c230e32463
BuildTools <unconfigured@null.spigotmc.org>
2025-02-26 17:50:06 +0000

    Initial Commit

---
+++ src/main/java/edu/canisius/csc213/project1/Card.java
@@ -0,0 +1,90 @@
+package main.java.edu.canisius.csc213.project1;
+
+import java.util.Objects;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Represents a playing card with a suit and rank.
+ */
+public class Card {
+
+    public enum Suit { HEARTS, DIAMONDS, CLUBS, SPADES }
+    public enum Rank { 
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, 
+        JACK, QUEEN, KING, ACE 
+    }
+
+    public Map<Rank, Integer> rankInts = new HashMap<Rank, Integer>(){{
+        put(Rank.TWO, 2);
+        put(Rank.THREE, 3);
+        put(Rank.FOUR, 4);
+        put(Rank.FIVE, 5);
+        put(Rank.SIX, 6);
+        put(Rank.SEVEN, 7);
+        put(Rank.EIGHT, 8);
+        put(Rank.NINE, 9);
+        put(Rank.TEN, 10);
+        put(Rank.JACK, 11);
+        put(Rank.QUEEN, 12);
+        put(Rank.KING, 13);
+        put(Rank.ACE, 14);
+
+    }};
+
+    // TODO: Define private fields for suit and rank.
+
+    private Suit suit;
+    private Rank rank;
+
+    // TODO: Implement the constructor.
+    public Card(Suit suit, Rank rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    // TODO: Implement getters for suit and rank.
+
+    public Suit getSuit(){
+        return suit;
+    }
+
+    public Rank getRank(){
+        return rank;
+    }
+
+    public int getRankInt(){
+        return rankInts.get(rank);
+    }
+
+
+    // TODO: Override toString() to return a readable format.
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
+    // TODO: Override equals() and hashCode() for comparisons.
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+
+        if(getClass() != obj.getClass()){
+            return false;
+        }
+
+        Card card = (Card) obj;
+        return suit == card.getSuit() && rank == card.getRank();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(suit, rank);
+    }
+}
---
+++ src/main/java/edu/canisius/csc213/project1/Deck.java
@@ -0,0 +1,58 @@
+package main.java.edu.canisius.csc213.project1;
+
+import java.util.ArrayList;
+
+/**
+ * Represents a deck of playing cards with a configurable size.
+ */
+public class Deck {
+    private final ArrayList<Card> cards = new ArrayList<Card>();
+
+    /**
+     * Creates a deck with a given size.
+     * The size must be a multiple of 4 and at most 52.
+     * 
+     * @param size The number of cards in the deck.
+     * @throws IllegalArgumentException if size is invalid.
+     */
+    public Deck(int size) {
+        // TODO: Validate size (must be a multiple of 4 and at most 52).
+        if(size % 4 != 0){
+            System.out.println("Invalid size, setting to 24");
+            size = 24;            
+        }
+
+        
+
+
+        // TODO: Initialize the deck with the correct cards.
+    }
+
+    /**
+     * Shuffles the deck.
+     */
+    public void shuffle() {
+        // TODO: Implement shuffle logic.
+    }
+
+    /**
+     * Draws the top card from the deck.
+     * 
+     * @return The drawn card.
+     * @throws NoSuchElementException if the deck is empty.
+     */
+    public Card draw() {
+        // TODO: Implement draw logic.
+        return null;
+    }
+
+    /**
+     * Gets the number of remaining cards in the deck.
+     *
+     * @return The number of cards left.
+     */
+    public int size() {
+        // TODO: Implement size method.
+        return 0;
+    }
+}
